da4ecd451236d84defc7fecfc5c9e7f959c7bab3
Rudraraju-Deepika <31842803+Rudraraju-Deepika@users.noreply.github.com>
2017-09-12 13:01:39 +0530

    my first commit

---
+++ main1.java
@@ -0,0 +1,13 @@
+public class Main {
+
+	public static void main(String[] args)
+
+        {
+          BufferedReader br=new BufferedReader(new inputSreamReader(System.in));
+          scanner input=br.readLine();
+            int original= input.length();
+ 
+           student getstudents[]=new students[10];
+	}
+
+}
